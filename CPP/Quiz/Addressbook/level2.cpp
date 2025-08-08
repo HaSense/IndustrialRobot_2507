8287892f0412e16378265e206fdad7c34702c830
Virtual Sense <virtual_sense@naver.com>
2025-08-08 14:54:59 +0900

    주소록 연속 입력
    입력 버퍼 문제 주의

--- CPP/Quiz/Addressbook/level2.cpp
+++ CPP/Quiz/Addressbook/level2.cpp
@@ -52,12 +52,12 @@ int main()
         cout << "번호를 입력해 주세요. : ";
         string num_str;
         getline(cin, num_str);
-        input_num = stoi(num_str); 
-        
-        cout << "데이터의 이름을 입력해 주세요. : ";
-        getline(cin, input_name); 
+        input_num = stoi(num_str);
 
-        cout << "데이터의 전화번호를 입력해 주세요. : ";
+        cout << "이름을 입력해 주세요. : ";
+        getline(cin, input_name);
+
+        cout << "전화번호를 입력해 주세요. : ";
         getline(cin, input_hp);
 
         Person person(input_num, input_name, input_hp);
@@ -65,8 +65,9 @@ int main()
 
         char cont;
         cout << "계속 입력하시겠습니까? (y/n) : ";
-        if (!(cin >> cont) || (cont != 'y' && cont != 'Y')) 
+        if (!(cin >> cont) || (cont != 'y' && cont != 'Y'))
             break;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //한줄 추가 버퍼 비우기
     }
 
     for (Person p : addressbook) {
