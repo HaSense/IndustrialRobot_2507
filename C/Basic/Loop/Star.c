15d5325aa919e6ff2ac8bd3f9a9f7f139ed20a21
Virtual Sense <virtual_sense@naver.com>
2025-11-03 16:29:48 +0900

    Create Star.c

---
+++ C/Basic/Loop/Star.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	//1단계
+	/*int N;
+	scanf("%d", &N);*/
+
+	for (int i = 0; i <5; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+	//2단계
+	for (int i = 0; i < 5; i++) {
+		for (int j = 5; j > i; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+
+	//3단계
+	for (int i = 0; i < 5; i++) {
+		for (int j = 4; j > i; j--) {
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+
+
+	return 0;
+}
