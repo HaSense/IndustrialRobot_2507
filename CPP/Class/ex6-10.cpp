04e6269e129a5e826c8a1feb3cf866774450b5a5
Virtual Sense <virtual_sense@naver.com>
2025-12-02 12:24:58 +0900

    Create ex6-10.cpp

---
+++ CPP/Class/ex6-10.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+#include <cmath>
+using namespace std;
+
+class MathUtils
+{
+public:
+    static int abs(int a){
+        if(a > 0)
+            return a;
+        else
+            return -a;  
+        //return a>0 ? a : -a;  //삼항연산자    
+    }
+    static int max(int a, int b){
+        return a > b ? a : b;    
+    }
+    static int min(int a, int b){
+        return a < b ? a : b;    
+    }       
+};
+int main()
+{
+    MathUtils mu;
+    mu.abs(-5);
+
+    cout << MathUtils::abs(-5) << endl;
+    cout << MathUtils::max(3, 7) << endl;
+    cout << MathUtils::min(3, 7) << endl;   
+    
+    
+    return 0;
+}
