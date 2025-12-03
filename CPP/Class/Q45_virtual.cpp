813940703692ca9cc0adb7ee7b71df6cb6a08831
Virtual Sense <virtual_sense@naver.com>
2025-12-03 11:05:34 +0900

    가상함수, vector 사용
    Updated comments to clarify memory management and object storage locations.

--- CPP/Class/Q45_virtual.cpp
+++ CPP/Class/Q45_virtual.cpp
@@ -30,10 +30,10 @@ public:
 
 int main()
 {
-    vector<Hero*> list;
-    unique_ptr<Hero> k = make_unique<Knight>();
-    unique_ptr<Hero> a = make_unique<Archer>();
-    unique_ptr<Hero> w = make_unique<Wizard>();
+    vector<Hero*> list; //stack 값 -- Hero객체들이 있는 주소들을 모은 꾸러미
+    unique_ptr<Hero> k = make_unique<Knight>(); // 객체가 heap -- 코딩하는 곳은 stack
+    unique_ptr<Hero> a = make_unique<Archer>(); // 객체가 heap -- a변수 stack a변수의 값이 주소 Archer객체가 있는 시작주소
+    unique_ptr<Hero> w = make_unique<Wizard>(); // 객체가 heap -- w변수는 값이 주소 -> Wizard객체가 있는 시작주소값
     
     list.push_back(k.get());
     list.push_back(a.get());
