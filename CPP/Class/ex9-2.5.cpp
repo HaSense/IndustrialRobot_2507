39ff3223b1ba78e0c10567e6c991fef87b4c8b67
Virtual Sense <virtual_sense@naver.com>
2025-12-03 11:52:54 +0900

    가상함수 연습, 객체 생성과 포인터, 참조, 스마트포인터

--- CPP/Class/ex9-2.5.cpp
+++ CPP/Class/ex9-2.5.cpp
@@ -27,16 +27,34 @@ public:
     }
 };
 //전역함수
-void paint(Shape* p){
-    p->draw();
-    delete p; // 동적할당된 메모리 해제
+// void paint(Shape* p){
+//     p->draw();
+//     delete p; // 동적할당된 메모리 해제
+// }
+// void paint(unique_ptr<Shape> p){ //auto 사용가능 
+//     p->draw();
+// }
+void paint(Shape& p){ //참조는 값을 전달받는다.
+    p.draw();
 }
 
 int main()
 {
-    paint(new Circle());
-    paint(new Rect());
-    paint(new Line());
+    // paint(new Circle());
+    // paint(new Rect());
+    // paint(new Line());
     
+    // paint(make_unique<Circle>());
+    // paint(make_unique<Rect>());
+    // paint(make_unique<Line>());
+    
+    Circle c;
+    Rect r;
+    Line l;
+    paint(c);
+    paint(r);
+    paint(l);
+
+
     return 0;
 }
