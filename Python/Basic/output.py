4e6c96f650feb1c24ba4afd2a5630beac8cf6a5d
Virtual Sense <virtual_sense@naver.com>
2025-12-05 14:40:50 +0900

    print 출력

---
+++ Python/Basic/output.py
@@ -0,0 +1,11 @@
+#하나만 출력한다.
+print('#하나만 출력합니다')
+print("Hello Python Programming...!")
+print()
+
+#여러줄을 출력합니다.
+print('#여러줄을 출력합니다')
+print(10, 20, 30, 40, 50)
+print('안녕하세요', '저의' '이름은' '홍길동입니다!')
+print()
+
