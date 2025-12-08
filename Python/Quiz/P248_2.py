befb167c2c49cebb67b65945a1243a6a37800124
Virtual Sense <virtual_sense@naver.com>
2025-12-08 16:52:23 +0900

    P248_2.py

--- Python/Quiz/P248_2.py
+++ Python/Quiz/P248_2.py
@@ -2,8 +2,10 @@ key_list = ['name', 'hp', 'mp', 'level']
 value_list = ['기사', 200, 30, 5]
 character = {}
 
-for key in key_list:
-    for value in value_list:
-        character[key] = value
+for i in range(len(key_list)):
+    for j in range(len(value_list)):
+        if i == j:           
+            character[key_list[i]] = value_list[j]
 
 print(character)
+
