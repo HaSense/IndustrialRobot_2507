8e736ac8d035ebffec164f40ca8d92cc060fc5d4
Virtual Sense <virtual_sense@naver.com>
2026-07-02 08:55:49 +0900

    CREATE try catch 예외처리버전

---
+++ SQL/SQLite/CRUD/python/02_create_v2.py
@@ -0,0 +1,34 @@
+"""
+Ubuntu 24.04 / WSL
+
+실행
+python3 create_student.py
+"""
+
+import sqlite3
+
+# student.db 열기 (없으면 자동 생성)
+conn = sqlite3.connect("student.db")
+
+# SQL 실행을 위한 커서 생성
+cursor = conn.cursor()
+
+# student 테이블 생성
+sql = """
+CREATE TABLE IF NOT EXISTS student (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    phone TEXT
+);
+"""
+
+# SQL 실행
+cursor.execute(sql)
+
+# 변경사항 저장
+conn.commit()
+
+print("student 테이블 생성 완료")
+
+# 데이터베이스 닫기
+conn.close()
