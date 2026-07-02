86dc9da9d17c552a73ec6d0403c14e49101b0a2c
Virtual Sense <virtual_sense@naver.com>
2026-07-02 10:26:51 +0900

    예외처리 삽입버전 - try catch

--- SQL/SQLite/CRUD/python/01_create_v2.py
+++ SQL/SQLite/CRUD/python/01_create_v2.py
@@ -7,28 +7,36 @@ python3 create_student.py
 
 import sqlite3
 
-# student.db 열기 (없으면 자동 생성)
-conn = sqlite3.connect("student.db")
+conn = None
 
-# SQL 실행을 위한 커서 생성
-cursor = conn.cursor()
+try:
+    # student.db 열기 (없으면 자동 생성)
+    conn = sqlite3.connect("student.db")
 
-# student 테이블 생성
-sql = """
-CREATE TABLE IF NOT EXISTS student (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    phone TEXT
-);
-"""
+    # SQL 실행을 위한 커서 생성
+    cursor = conn.cursor()
 
-# SQL 실행
-cursor.execute(sql)
+    # student 테이블 생성
+    sql = """
+    CREATE TABLE IF NOT EXISTS student (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        phone TEXT
+    );
+    """
 
-# 변경사항 저장
-conn.commit()
+    # SQL 실행
+    cursor.execute(sql)
 
-print("student 테이블 생성 완료")
+    # 변경사항 저장
+    conn.commit()
 
-# 데이터베이스 닫기
-conn.close()
+    print("student 테이블 생성 완료")
+
+except sqlite3.Error as e:
+    print("테이블 생성 실패 :", e)
+
+finally:
+    # 데이터베이스 닫기
+    if conn:
+        conn.close()
